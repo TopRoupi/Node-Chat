e64dbc94f3e4aece6ba5bd6821d807b9ecb11d97
Breno Nunes <breno.nunesgalvao@gmail.com>
2018-08-01 18:05:45 -0300

    chat v1

--- app.js
+++ app.js
@@ -10,12 +10,22 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
+io.emit('some event', { for: 'everyone' });
+
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.broadcast.emit('hi');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on :3000');
